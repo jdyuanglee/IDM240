59f68e05588a4ce9fd2a86f064236ce4ec78ed57
jdyuanglee <yuanglee39@gmail.com>
2018-08-17 22:49:14 -0400

    Add final project

--- final/main.js
+++ final/main.js
@@ -1,21 +1,29 @@
+// Audio Playing & Wave animation
 var audioPlayerObj = document.getElementById('audioplayer');
 var bugleObj = document.getElementById('bugle');
 
-
 var sndPlaying = false;
 
+var mic = document.getElementById('mic');
+var micInside = document.getElementById('micInside');
+var micOutside = document.getElementById('micOutside');
 
 function audioToggle() {
-	if (sndPlaying) {
+	if (sndPlaying == true) {
 		audioPlayerObj.pause();
 		sndPlaying = false;
+		micInside.style.display = "none";
+		micOutside.style.display = "none";
 	} else {
+		console.log('playing');
 		audioPlayerObj.play();
 		sndPlaying = true;
+		micInside.style.display = "inline";
+		micOutside.style.display = "inline";
 	}
 }
 
-
+// Button hovered animations
 var button1 = document.getElementById('button1');
 var button2 = document.getElementById('button2');
 var button3 = document.getElementById('button3');
@@ -51,7 +59,31 @@ function noanimation3(){
 }
 function animation4(){
 	compass.classList.add('ishovered4');
+	compass.classList.remove('ishovered5');
 }
 function noanimation4(){
 	compass.classList.remove('ishovered4');
+	compass.classList.add('ishovered5');
+}
+
+// Night shift
+var day = document.getElementById('day');
+var night = document.getElementById	('night');
+var header = document.querySelector('header');
+
+var isday = true;
+
+function nightshift(){
+	if (isday == true){
+		isday = false;
+		night.style.visibility = "visible";
+		day.style.visibility = "hidden";
+		header.style.backgroundImage = "url('assets/6.jpg')";
+	}
+	else{
+		isday = true;
+		night.style.visibility = "hidden";
+		day.style.visibility = "visible";
+		header.style.backgroundImage = "url('assets/7.jpg')";
+	}
 }
